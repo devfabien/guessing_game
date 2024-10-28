079f525cef457d42dc4b2f6fb33b1a806d40cb26
devfabien <ish001fabi@gmail.com>
2024-10-28 18:56:49 +0200

    feat: generating a random number

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number! ");
     println!("\nPlease input your guess.");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    //let secret_number: u32 = random() % 100 + 1; // This is the same as the above line
 
     let mut guess = String::new();
 
@@ -11,4 +15,5 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+    println!("The secret number is: {}", secret_number);
 }
