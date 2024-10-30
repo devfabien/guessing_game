bf6e46810ac262835b5f58f76704af793636bd73
devfabien <ish001fabi@gmail.com>
2024-10-30 09:42:36 +0200

    feat: add the implementation to convert a recieved string to a number

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,16 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
     //let secret_number: u32 = random() % 100 + 1; // This is the same as the above line
+    println!("The secret number is: {}", secret_number);
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
     println!("You guessed: {}", guess);
-    println!("The secret number is: {}", secret_number);
+    // println!("The secret number is: {}", secret_number);
 
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("This number is small!"),
