d94140d865ef6e251d0aa8dde1469c242cd1ce1a
devfabien <ish001fabi@gmail.com>
2024-10-30 09:53:09 +0200

    feat: add the implementation to allow multiple guessing until the user wins

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,32 @@ use std::io;
 
 fn main() {
     println!("Guess the number! ");
-    println!("\nPlease input your guess.");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
     //let secret_number: u32 = random() % 100 + 1; // This is the same as the above line
     println!("The secret number is: {}", secret_number);
-    let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    loop {
+        println!("\nPlease input your guess.");
 
-    println!("You guessed: {}", guess);
-    // println!("The secret number is: {}", secret_number);
+        let mut guess = String::new();
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("This number is small!"),
-        Ordering::Greater => println!("This number is greater!"),
-        Ordering::Equal => println!("You win!"),
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {}", guess);
+        // println!("The secret number is: {}", secret_number);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("This number is small!"),
+            Ordering::Greater => println!("This number is greater!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
